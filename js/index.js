6058d6da8db8f6ad4b4c8a73a99e6ff5a88f6d8d
unknown <nikitabghute7350@gmail.com>
2026-02-24 01:04:41 +0530

    smart expense tracker project

---
+++ js/index.js
@@ -0,0 +1,71 @@
+//step 1- get elements from html doc/dom
+// get values of total details
+const rem_balance=document.getElementById('balance');
+const currMonth=document.getElementById('month_total');
+const trans_count=document.getElementById('count');
+// get elements from form
+const trans_amount=document.getElementById('amount')
+const select_type=document.getElementById('type');
+const categoryInput = document.getElementById("category");
+const dateInput = document.getElementById("date");
+const noteInput = document.getElementById("note");
+const form=document.getElementById("transaction_form");
+// gets elements from 
+const balanceEl = document.getElementById("balance");
+const monthTotalEl = document.getElementById("month_total");
+const countEl = document.getElementById("count");
+// get elements of transaction list generated
+const expense_list = document.getElementById("expenseList");
+
+form.addEventListener('submit', (e)=>{
+   e.preventDefault();       //stop page refresh 
+// step 2-get values of all fields of form
+   const amount1=trans_amount.value;
+   console.log("transaction amount", amount1)
+   const trans_type= select_type.value;
+   console.log("type of transaction:", trans_type)
+   const trans_category=category.value;
+   console.log('catogery of transcation:',trans_category)
+   const trans_date=dateInput.value;
+   console.log("date of transaction is:", trans_date)
+   const trans_note=noteInput.value
+   console.log("note on transaction:", trans_note)
+   
+   //access values of all fields
+   // step 3- create object of every transation, because every transaction has many details, so can store in the ject form
+   const expense={
+      id:Date.now,
+      amount1:amount1,
+      trans_type:trans_type,
+      trans_category:trans_category,
+      trans_date:trans_date,
+      trans_note:trans_note,
+   }
+   console.log("expense object", expense)
+   expenses.push(expense);   //push expense objects into expenses array
+   console.log("expenses pushed", expenses)
+   console.log("Total expenses", expenses.length);
+   form.reset();
+
+   // funciton to render new list after chick on submit
+   expenses.push(expense);
+   renderExpenses();
+})
+   
+// step 4- create array once you compelted step 3, outside of submit button so that it will remember all expenses
+    //and push expense object into expenses values.
+    let expenses=[];
+   console.log("expenses array", expenses)
+
+   // step 5- create lists of all expenses and render(displays)
+   console.log("expense_list", expense_list)
+
+ let renderExpenses=()=>{
+   expense_list.innerHTML='';
+   expenses.forEach(()=>{
+      const new_row=document.createElement("li");
+      console.log("row from loop", new_row)
+      expense_list.appendChild(new_row);
+      });
+      console.log("expenses", expenses)
+ }
